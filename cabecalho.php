a73bb54b23a490489c57dbe63f92ae200060e728
henrique rojo <henriquerojo10@gmail.com>
2023-02-28 16:55:02 -0300

    navegação com display flex

--- cabecalho.php
+++ cabecalho.php
@@ -1,10 +1,23 @@
-<nav>
+<header class="area_cabecalho">
+
+
+  <div class="w50">
+    <img src="img/logo2.png" alt="" class="imagem">
+  </div>
+
+  <nav class="w50">
   <ul>
-    <img src="" alt="logo da empresa">
     <li><a href="#">Home</a></li>
     <li><a href="#">Produtos</a></li>
     <li><a href="#">Missão</a></li>
     <li><a href="#">Links</a></li>
     <li><a href="#">Contato</a></li>
-  </ul> 
-</nav>
+  </ul>
+  </nav>
+
+</header>
+
+
+
+
+
