d2e3da466ae5842c011a213212823c9bf35c0be9
henrique rojo <henriquerojo10@gmail.com>
2023-03-06 17:02:22 -0300

    melhorando estética do sobre conforme wireframe

--- sobre.php
+++ sobre.php
@@ -1,11 +1,15 @@
 <section class="fundocinza" id="sobre">
           <div>
-              <h1 class="">Sobre Nós</h1>
-              <section class="">
+              <h1>Sobre Nós</h1>
+              <section>
                 <img src="img/linha.svg" alt="linha decorativa do texto" class="linhadsobre">
               </section>
               <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita voluptates pariatur harum dignissimos aperiam similique quis cupiditate eos ad! Atque eveniet ratione earum quos, odit provident repellendus quisquam. Quam, ex?
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              <br>
+              Expedita voluptates pariatur harum dignissimos aperiam similique quis cupiditate eos ad! Atque eveniet ratione
+              <br> 
+              earum quos, odit provident repellendus quisquam. Quam, ex?
               </p>
           </div>
           <div> <!-- LOGO DA EMPRESA -->
