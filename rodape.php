9db8efd7f8229541482dd4c20d70cee4d44b256c
henrique rojo <henriquerojo10@gmail.com>
2023-03-06 23:34:26 -0300

    correção container contato e início do formulario

--- rodape.php
+++ rodape.php
@@ -1,18 +1,34 @@
-<div id="imagem_contato">
-    <img src="img/ar_condicionado.png" alt="imagem de arcondicionado" class="imagem_contato">
-  </div>
-  <div id="formulario">
-    <h1 class="alinhar">Contato</h1>
-    <div class="alinhar">
-      <img src="img/linha.svg" alt="linha decorativa do texto" class="linhadcontato">
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <section id="contato">
+    <div id="imagem_contato">
+      <img src="img/ar_condicionado.png" alt="imagem ar condicionado" class="imagem_contato">
     </div>
-    <form action="submit">
-        <input type="text" ng-model="nome" placeholder="Digite seu Nome">
-        <input type="text" ng-model="email" placeholder="Digite seu E-mail">
-    </form>
-    <form>
-        <textarea name="mensagem" id="mensagem" rows="1" placeholder="Mensagem"></textarea>
-    </form>
-    <input type="button" action="submit" value="Enviar"> 
-  </div>
-  
+    <article>
+      <p>Contato</p>
+      <img src="img/linha.svg" alt="linha decorativa do texto" class="linhadcontato">
+      <div>
+        <h1>ENDEREÇO</h1>
+      </div>
+      <div id="formulario">
+      <form action="submit">
+          <input type="text" ng-model="nome" placeholder="Digite seu Nome" class="borda">
+          <input type="text" ng-model="email" placeholder="Digite seu E-mail" class="borda">
+      </form>
+      <form>
+          <textarea name="mensagem" id="mensagem" rows="1" placeholder="Mensagem"></textarea>
+      </form>
+      <input type="button" action="submit" value="Enviar"> 
+    </div>
+    </article>
+    
+  </section>
+    
+</body>
+</html>
