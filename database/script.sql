51e8170fe2ffa79bb7a68e6c9695bd04715f45c2
henrique rojo <henriquerojo10@gmail.com>
2023-03-07 07:33:26 -0300

    início insert dos produtos

--- database/script.sql
+++ database/script.sql
@@ -27,4 +27,26 @@ insert tbtipos values (4, 'energiasolar', 'ens');
 
 select * from tbtipos;
 
-insert tbprodutos values ()
+insert tbprodutos values (1);
+insert tbprodutos values (2);
+insert tbprodutos values (3);
+insert tbprodutos values (4);
+insert tbprodutos values (5);
+
+insert tbprodutos values (6);
+insert tbprodutos values (7);
+insert tbprodutos values (8);
+insert tbprodutos values (9);
+insert tbprodutos values (10);
+
+insert tbprodutos values (11);
+insert tbprodutos values (12);
+insert tbprodutos values (13);
+insert tbprodutos values (14);
+insert tbprodutos values (15);
+
+insert tbprodutos values (16);
+insert tbprodutos values (17);
+insert tbprodutos values (18);
+insert tbprodutos values (19);
+insert tbprodutos values (20);
