21f998b049968f25f8a3c40556a1be5682feb333
henrique rojo <henriquerojo10@gmail.com>
2023-03-07 08:13:36 -0300

    adição comandos script

--- database/script.sql
+++ database/script.sql
@@ -1,4 +1,7 @@
 CREATE DATABASE dbbar;
+
+show databases;
+
 USE dbbar;
 
 create table tbtipos(
@@ -33,11 +36,15 @@ insert tbprodutos values (3,'Ar Condicionado 10.000 BTU','Ar Condicionado com ca
 insert tbprodutos values (4,'Ar Condicionado 11.000 BTU','Ar Condicionado com capacidade de refrigeração de 11.000 BTU',2500.00,'ar4.jpg',1);
 insert tbprodutos values (5,'Ar Condicionado 12.000 BTU','Ar Condicionado com capacidade de refrigeração de 12.000 BTU',3000.00,'ar5.jpg',1);
 
+select * from tbprodutos where id_tipo = 1;
+
 insert tbprodutos values (6,'Inverter 8.000 BTU','Inverter com capacidade de refrigeração de 8.000 BTU',1200.00,'inverter1.jpg',2);
 insert tbprodutos values (7,'Inverter 9.000 BTU','Inverter com capacidade de refrigeração de 9.000 BTU',1700.00,'inverter2.jpg',2);
 insert tbprodutos values (8,'Inverter 10.000 BTU','Inverter com capacidade de refrigeração de 10.000 BTU',2200.00,'inverter3.jpg',2);
 insert tbprodutos values (9,'Inverter 11.000 BTU','Inverter com capacidade de refrigeração de 11.000 BTU',2700.00,'inverter4.jpg',2);
 insert tbprodutos values (10,'Inverter 12.000 BTU','Inverter com capacidade de refrigeração de 12.000 BTU',3200.00,'inverter5.jpg',2);
+
+select * from tbprodutos where id_tipo = 2;
 
 insert tbprodutos values (11,'Multisplit 8.000 BTU','Multisplit com capacidade de refrigeração de 8.000 BTU',1100.00,'multisplit1.jpg',3);
 insert tbprodutos values (12,'Multisplit 9.000 BTU','Multisplit com capacidade de refrigeração de 9.000 BTU',1600.00,'multisplit2.jpg',3);
@@ -45,8 +52,17 @@ insert tbprodutos values (13,'Multisplit 10.000 BTU','Multisplit com capacidade 
 insert tbprodutos values (14,'Multisplit 11.000 BTU','Multisplit com capacidade de refrigeração de 11.000 BTU',2600.00,'multisplit4.jpg',3);
 insert tbprodutos values (15,'Multisplit 12.000 BTU','Multisplit com capacidade de refrigeração de 12.000 BTU',3100.00,'multisplit5.jpg',3);
 
+select * from tbprodutos where id_tipo = 3;
+
 insert tbprodutos values (16,'Placa solar 300W','Placar solar com potência de 300W',700.00,'solar.webp',4);
 insert tbprodutos values (17,'Placa solar 400W','Placar solar com potência de 400W',800.00,'solar.webp',4);
 insert tbprodutos values (18,'Placa solar 500W','Placar solar com potência de 500W',900.00,'solar.webp',4);
 insert tbprodutos values (19,'Placa solar 600W','Placar solar com potência de 600W',979.99,'solar.webp',4);
 insert tbprodutos values (20,'Placa solar 700W','Placar solar com potência de 700W',1050.00,'solar.webp',4);
+
+select * from tbprodutos where id_tipo = 4;
+
+desc tbtipos;
+desc tbprodutos;
+select * from tbtipos;
+select * from tbprodutos;
