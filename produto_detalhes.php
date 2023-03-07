5cc06d4f4be27b1f46aeb90e33c4e6cf896188c1
henrique rojo <henriquerojo10@gmail.com>
2023-03-07 08:29:49 -0300

    teste de link com banco

--- produto_detalhes.php
+++ produto_detalhes.php
@@ -1,5 +1,7 @@
 <?php 
 include 'conn/connect.php';
-
-
+$busca = $_GET['buscar'];
+$lista = $conn->query("select * from tbprodutos where imagem like '%$busca%';");
+$row_produto = $lista->fetch_assoc();
+$num_linhas = $lista->num_rows;
 ?>
