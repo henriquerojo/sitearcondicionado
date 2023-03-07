ea18112cbc2bdb5ebaaa44ee4c7be8e3f028b528
henrique rojo <henriquerojo10@gmail.com>
2023-03-07 07:41:32 -0300

    adição de chaves estrangeiras no insert de produto

--- database/script.sql
+++ database/script.sql
@@ -27,26 +27,26 @@ insert tbtipos values (4, 'energiasolar', 'ens');
 
 select * from tbtipos;
 
-insert tbprodutos values (1,'','','','','');
-insert tbprodutos values (2,'','','','','');
-insert tbprodutos values (3,'','','','','');
-insert tbprodutos values (4,'','','','','');
-insert tbprodutos values (5,'','','','','');
+insert tbprodutos values (1,'','','','',1);
+insert tbprodutos values (2,'','','','',1);
+insert tbprodutos values (3,'','','','',1);
+insert tbprodutos values (4,'','','','',1);
+insert tbprodutos values (5,'','','','',1);
 
-insert tbprodutos values (6,'','','','','');
-insert tbprodutos values (7,'','','','','');
-insert tbprodutos values (8,'','','','','');
-insert tbprodutos values (9,'','','','','');
-insert tbprodutos values (10,'','','','','');
+insert tbprodutos values (6,'','','','',2);
+insert tbprodutos values (7,'','','','',2);
+insert tbprodutos values (8,'','','','',2);
+insert tbprodutos values (9,'','','','',2);
+insert tbprodutos values (10,'','','','',2);
 
-insert tbprodutos values (11,'','','','','');
-insert tbprodutos values (12,'','','','','');
-insert tbprodutos values (13,'','','','','');
-insert tbprodutos values (14,'','','','','');
-insert tbprodutos values (15,'','','','','');
+insert tbprodutos values (11,'','','','',3);
+insert tbprodutos values (12,'','','','',3);
+insert tbprodutos values (13,'','','','',3);
+insert tbprodutos values (14,'','','','',3);
+insert tbprodutos values (15,'','','','',3);
 
-insert tbprodutos values (16,'','','','','');
-insert tbprodutos values (17,'','','','','');
-insert tbprodutos values (18,'','','','','');
-insert tbprodutos values (19,'','','','','');
-insert tbprodutos values (20,'','','','','');
+insert tbprodutos values (16,'','','','',4);
+insert tbprodutos values (17,'','','','',4);
+insert tbprodutos values (18,'','','','',4);
+insert tbprodutos values (19,'','','','',4);
+insert tbprodutos values (20,'','','','',4);
