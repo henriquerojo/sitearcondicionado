05f24fe0c0afac5216725185d4fbff88bfd5b26b
henrique rojo <henriquerojo10@gmail.com>
2023-03-07 08:07:48 -0300

    inserção multisplit 100%

--- database/script.sql
+++ database/script.sql
@@ -39,11 +39,11 @@ insert tbprodutos values (8,'Inverter 10.000 BTU','Inverter com capacidade de re
 insert tbprodutos values (9,'Inverter 11.000 BTU','Inverter com capacidade de refrigeração de 11.000 BTU',2700.00,'inverter4.jpg',2);
 insert tbprodutos values (10,'Inverter 12.000 BTU','Inverter com capacidade de refrigeração de 12.000 BTU',3200.00,'inverter5.jpg',2);
 
-insert tbprodutos values (11,'Multisplit 8.000 BTU','Multisplit com capacidade de refrigeração de 8.000 BTU',1100.00,'',3);
-insert tbprodutos values (12,'Multisplit 9.000 BTU','Multisplit com capacidade de refrigeração de 9.000 BTU',1600.00,'',3);
-insert tbprodutos values (13,'Multisplit 10.000 BTU','Multisplit com capacidade de refrigeração de 10.000 BTU',2100.00,'',3);
-insert tbprodutos values (14,'Multisplit 11.000 BTU','Multisplit com capacidade de refrigeração de 11.000 BTU',2600.00,'',3);
-insert tbprodutos values (15,'Multisplit 12.000 BTU','Multisplit com capacidade de refrigeração de 12.000 BTU',3100.00,'',3);
+insert tbprodutos values (11,'Multisplit 8.000 BTU','Multisplit com capacidade de refrigeração de 8.000 BTU',1100.00,'multisplit1.jpg',3);
+insert tbprodutos values (12,'Multisplit 9.000 BTU','Multisplit com capacidade de refrigeração de 9.000 BTU',1600.00,'multisplit2.jpg',3);
+insert tbprodutos values (13,'Multisplit 10.000 BTU','Multisplit com capacidade de refrigeração de 10.000 BTU',2100.00,'multisplit3.jpg',3);
+insert tbprodutos values (14,'Multisplit 11.000 BTU','Multisplit com capacidade de refrigeração de 11.000 BTU',2600.00,'multisplit4.jpg',3);
+insert tbprodutos values (15,'Multisplit 12.000 BTU','Multisplit com capacidade de refrigeração de 12.000 BTU',3100.00,'multisplit5.jpg',3);
 
 insert tbprodutos values (16,'Placa solar 300W','Placar solar com potência de 300W',700.00,'',4);
 insert tbprodutos values (17,'Placa solar 400W','Placar solar com potência de 400W',800.00,'',4);
