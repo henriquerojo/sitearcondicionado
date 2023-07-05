69b0b37512eeaabe9f894d32b2ed7f377062867e
henriquerojo <henriquerojo10@gmail.com>
2023-07-05 16:12:36 -0300

    finalização missão

--- missao.php
+++ missao.php
@@ -1,17 +1,17 @@
-<div class="alinhar fundo_amarelo">
-          <div>
-            <img src="img/medalhinha.jpg" alt="" class="medalha">
-            <h4>Missão</h4>
-            <p>Nossa missão é bla bla bla</p>
-          </div>
-          <div>
-            <img src="img/medalhinha.jpg" alt="" class="medalha">
-            <h4>Visão</h4>
-            <p>Nossa visão é bla bla bla</p>
-          </div>
-          <div>
-            <img src="img/medalhinha.jpg" alt="" class="medalha">
-            <h4>Valores</h4>
-            <p>Nossos valores são bla bla bla</p>
-          </div>
+<div class="alinha-missao fundo_amarelo">
+  <div>
+    <img src="img/medalhinha.jpg" alt="" class="medalha">
+    <h4>Missão</h4>
+    <p>Texto</p>
+  </div>
+  <div>
+    <img src="img/medalhinha.jpg" alt="" class="medalha">
+    <h4>Visão</h4>
+    <p>Texto</p>
+  </div>
+  <div>
+    <img src="img/medalhinha.jpg" alt="" class="medalha">
+    <h4>Valores</h4>
+    <p>Texto</p>
+  </div>
 </div>
